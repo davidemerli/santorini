c476ad70fea4c0356ca31b44276c47c32f4a9c89
davidemerli <davide.merli@outlook.com>
2020-03-25 22:40:37 +0100

    added basic immutable Map structure

--- src/main/java/it/polimi/ingsw/PSP1/santorini/map/Map.java
+++ src/main/java/it/polimi/ingsw/PSP1/santorini/map/Map.java
@@ -2,16 +2,38 @@ package it.polimi.ingsw.PSP1.santorini.map;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Map {
+
+    private final int SIDE_LENGTH = 5;
 
     private final SquareData[][] blockMatrix;
     private final List<Worker> workersList;
 
+    /**
+     * Main constructor for the Map class
+     * Used to instantiate an empty map with no workers and every square set to level 0
+     */
     public Map() {
-        this.blockMatrix = new SquareData[5][5];
+        this.blockMatrix = new SquareData[SIDE_LENGTH][SIDE_LENGTH];
         this.workersList = new ArrayList<>();
+        initMatrix();
+    }
+
+    /**
+     * Copy constructor
+     * Used to instantiate a new 'step' of the map, based on the previous one, since Map is immutable
+     * Both workers and square data are copied.
+     *
+     *
+     * @param oldMap where the current state is copied from.
+     */
+    private Map(Map oldMap) {
+        this.blockMatrix = oldMap.getMatrixCopy();
+        this.workersList = oldMap.getWorkersCopy();
     }
 
     public Map addWorker(Worker worker) {
@@ -42,5 +64,46 @@ public class Map {
         //TODO
 
         return null;
+    }
+
+    /**
+     * Instantiate a new ArrayList of cloned Workers, to keep everything immutable
+     *
+     * @return a perfect clone of the current array of Workers
+     */
+    private List<Worker> getWorkersCopy() {
+        return workersList.stream()
+                .map(w -> new Worker(w.getPlayer(), w.getPosition()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Instantiate a new matrix of cloned Squares, to keep everything immutable
+     *
+     * @return a perfect clone of the current SquareData matrix
+     */
+    private SquareData[][] getMatrixCopy() {
+        SquareData[][] newMatrix = new SquareData[SIDE_LENGTH][SIDE_LENGTH];
+
+        for (int i = 0; i < SIDE_LENGTH; i++) {
+            for (int j = 0; j < SIDE_LENGTH; j++) {
+                SquareData squareToCopy = blockMatrix[i][j];
+
+                newMatrix[i][j] = new SquareData(squareToCopy.getLevel(), squareToCopy.isDome());
+            }
+        }
+
+        return newMatrix;
+    }
+
+    /**
+     * Initializes the square data matrix with all level 0 values
+     */
+    private void initMatrix() {
+        for (int i = 0; i < SIDE_LENGTH; i++) {
+            for (int j = 0; j < SIDE_LENGTH; j++) {
+                blockMatrix[i][j] = new SquareData(0, false);
+            }
+        }
     }
 }
